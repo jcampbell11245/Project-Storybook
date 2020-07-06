7e91891f96238e15ea18122c13bbbb052d0d2e15
jcampbell11245 <47761573+jcampbell11245@users.noreply.github.com>
2020-07-05 21:25:18 -0400

    Implemented ducking and dodging

--- Player2D.gd
+++ Player2D.gd
@@ -7,7 +7,6 @@ export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var acceleration = 0.25
 var velocity = Vector2.ZERO
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,6 +16,7 @@ func _ready():
 #	pass
 
 func get_input():
+	#Left and right movement
 	var dir = 0
 	if Input.is_action_pressed("move_right"):
 		dir += 1
@@ -26,7 +26,26 @@ func get_input():
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
+	
+	#Ducking
+	if Input.is_action_just_pressed("duck"):
+		$AnimationManager.play("duck", false)
+		$CollisionShape.shape.extents = Vector2(32, 32)
+		move_local_y(32)
+		speed = 400
+	if Input.is_action_just_released("duck"):
+		$AnimationManager.play("idle", false)
+		$CollisionShape.shape.extents = Vector2(32, 64)
+		speed = 850
 		
+	#Dodging
+	if(Input.is_action_just_pressed("dodge_right")) && $DodgeTimer.time_left == 0:
+		$DodgeTimer.start(1.0)
+		velocity.x += 1500
+	if(Input.is_action_just_pressed("dodge_left")) && $DodgeTimer.time_left == 0:
+		$DodgeTimer.start(1.0)
+		velocity.x -= 1500
+	
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
